1315f58e1c53511735c2a5592872ca865e7116fc
sengendo Nazil <ssengendonazil@gmail>
2026-01-19 17:41:03 +0300

    add more headers

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
-import { pomPinia,setBearerToken,getBearerToken } from "septor-store"
+import { pomPinia, setBearerToken, getBearerToken } from "septor-store"
 import { onMounted } from "vue";
 
-setBearerToken({token:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YyL2F1dGgvMmZhIiwiaWF0IjoxNzQ3Mzc5NTc2LCJleHAiOjE3NDc0MjI3NzYsIm5iZiI6MTc0NzM3OTU3NiwianRpIjoiUWxuQjltb0F2anp6cXRxNSIsInN1YiI6IjIyIiwicHJ2IjoiY2I0ZWU5OTdiYjIyNTEyMTg0M2NiMmU1M2I3NGM2M2FkM2RlN2I0YiJ9.l7vaIIWhpEcAAKW4b5MKG2ET8AQnmcAcAw1Wr1fBBQg'})
+setBearerToken({ token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YyL2F1dGgvMmZhIiwiaWF0IjoxNzQ3Mzc5NTc2LCJleHAiOjE3NDc0MjI3NzYsIm5iZiI6MTc0NzM3OTU3NiwianRpIjoiUWxuQjltb0F2anp6cXRxNSIsInN1YiI6IjIyIiwicHJ2IjoiY2I0ZWU5OTdiYjIyNTEyMTg0M2NiMmU1M2I3NGM2M2FkM2RlN2I0YiJ9.l7vaIIWhpEcAAKW4b5MKG2ET8AQnmcAcAw1Wr1fBBQg' })
 
 const state = pomPinia()
 const fetchData = async () => {
-      const data = await state.stateGenaratorApi({
-        reload: true,// if false ,Once the data is colleted  never call again  And if true   call the data in all stuation
-        StateStore: 'users11',
-        reqs: { url: 'v3/patients?Rows=100&status=1&page=1&both=1&branchId=1', method: 'get',data:{} }, //data:{sex:male} is not required
-        time: 5,// if the state not empty  time taken to recall again the api
-      });
-      console.log('Fetched data:', data);
+  const data = await state.stateGenaratorApi({
+    reload: true,// if false ,Once the data is colleted  never call again  And if true   call the data in all stuation
+    StateStore: 'users11',
+    reqs: {
+      url: 'v3/patients?Rows=100&status=1&page=1&both=1&branchId=1', method: 'get', data: {},
+    }, //data:{sex:male} is not required
+    config:{
+      headers: {
+        "X-request_date_client": Date.now(),
+        "X-app_key": "MERCHANT_MANAGE_WEB_PC",
+        "X-token_id": "7867687687 whkaj adjk",
+        "X-owner_id": "7867687687 whkaj adjk",
+        "X-owner_type": "7867687687 whkaj adjk"
+      }
+    },
+    time: 5,// if the state not empty  time taken to recall again the api
+  });
+  console.log('Fetched data:', data);
 };
 
 
@@ -31,16 +42,16 @@ onMounted(() => {
   <div class="card">
     <button type="button" @click="state.counter++">count is {{ state.counter }}</button>
     <p>
-         <ul>
-        <li >{{ state.users11 }}</li>
-      </ul>
-      <ul>
-        <li v-for="counts in (state.counter??[])"></li>
-      </ul>
-      <code>components/HelloWorld.vue</code> to test HMR
+    <ul>
+      <li>{{ state.users11 }}</li>
+    </ul>
+    <ul>
+      <li v-for="counts in (state.counter ?? [])"></li>
+    </ul>
+    <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
- 
+
 </template>
 
 <style scoped>
